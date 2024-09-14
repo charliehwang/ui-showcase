672d812eec1c004903773b9e6b235b2f82382015
charlie <charliehwang@gmail.com>
2024-09-13 20:19:10 -0600

    test: add a11y for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,6 +14,7 @@ const config: StorybookConfig = {
         },
       },
     },
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/nextjs",
--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -6,8 +6,8 @@ import { Button } from "./Button";
 const meta = {
   title: "Components/Button",
   component: Button,
+  // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
